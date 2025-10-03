f8c54b5f6f7dadbf4b0b7acb140294416ade6569
Charles Ueltschey <chaseuelt@gmail.com>
2025-10-03 14:48:13 -0500

    Fix gains, fix iperf IP

--- ue.sh
+++ ue.sh
@@ -9,6 +9,6 @@ set -x
 
 rtue_id=$(docker ps | awk '/ghcr.io\/oran-testing\/rtue/{print $1}')
 
-docker exec $rtue_id bash -c "ip ro add 10.53.0.0/16 via 10.45.0.1"
+docker exec $rtue_id bash -c "ip ro add 10.53.0.0/16 via 10.45.1.1"
 
-docker exec -it $rtue_id bash -c "iperf3 -c 10.53.1.1 -u -i 1 -t 36000 -b 3M"
+docker exec -it $rtue_id bash -c "iperf3 -c 10.53.1.1 -u -i 1 -t 36000 -b 0.05M"
